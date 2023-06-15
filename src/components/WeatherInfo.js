d2c12e8ec59036a0bbe79437feadbfd2b3513626
thanushan96 <ravishankarthanushan96@gmail.com>
2023-06-15 10:02:20 +0530

    Moved constant variables

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -2,55 +2,49 @@ import React, { useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import '../css/style.css';
 import { citiesData } from '../data/citiesData';
-import { apiKey, baseUrl, unitType } from '../constants/apiHelper';
+// import { apiKey, baseUrl, unitType } from '../constants/apiHelper';
 import { processData } from '../constants/helper';
 
+import { getCityCodes, generateUrl } from '../constants/apiHelper';
+
+
+
+
+// Modification start 
 
 
 const WeatherInfo = () => {
   console.log('Component rendering!');
+
   const fetchWeatherData = async () => {
-    const cityCodes = citiesData.List.map(city => city.CityCode);
-    const url = `${baseUrl}?id=${cityCodes.join(',')}&units=${unitType}&appid=${apiKey}`;
+    const cityCodes = getCityCodes(citiesData);
+    const url = generateUrl(cityCodes);
 
     try {
       const response = await fetch(url);
       const weatherData = await response.json();
-      return processData(weatherData, citiesData);
+      const weatherCards = processData(weatherData, citiesData);
+      return weatherCards;
     } catch (error) {
       console.error('Error fetching weather data:', error);
       return [];
     }
   };
 
-  
-
-
   useEffect(() => {
     console.log('Component is unmounting or condition changed!');
-    fetchWeatherData()
-      .then(weatherCards => {
-        const weatherContainer = document.getElementById('weather-container');
-        ReactDOM.render(weatherCards, weatherContainer);
-      });
-      // eslint-disable-next-line react-hooks/exhaustive-deps
+    fetchWeatherData().then(weatherCards => {
+      const weatherContainer = document.getElementById('weather-container');
+      ReactDOM.render(weatherCards, weatherContainer);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-
-  
   return (
     <div>
-      
-         
-    <div id="weather-container"></div>
-      
-
+      <div id="weather-container"></div>
     </div>
   );
 };
 
 export default WeatherInfo;
-
-
-
-// change below
--- src/constants/apiHelper.js
+++ src/constants/apiHelper.js
@@ -2,4 +2,16 @@ const apiKey = process.env.REACT_APP_API_KEY || '5a860fd4bdc18e1897feaaf0c8a30a6
 const baseUrl = 'http://api.openweathermap.org/data/2.5/group';
 const unitType = 'metric';
 
+
+export const getCityCodes = (citiesData) => {
+    return citiesData.List.map(city => city.CityCode);
+  };
+  
+  export const generateUrl = (cityCodes) => {
+    return `${baseUrl}?id=${cityCodes.join(',')}&units=${unitType}&appid=${apiKey}`;
+  };
+  
+
+
+
 export { apiKey, baseUrl, unitType };
